8b40edfd8d1688a83e0da9bc627eae719105b992
Sylwia <slw.krupa@gmail.com>
2019-11-25 19:16:58 +0100

    my first files

---
+++ main.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <title>Graduades</title>
+
+    <meta charset="utf-8">   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<header>
+    <h3>Data of graduated</h3>
+    <div>
+    <ul>
+        <li><a href="main.php">Home</a></li>
+        <li><a href="main.php">Add a branch</a></li>
+        <li><a href="main.php">Add graduate</a></li>
+        <li><a href="main.php">Database</a></li>
+        <li><a href="main.php">Resume</a></li>
+    </ul>
+    </div>
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
